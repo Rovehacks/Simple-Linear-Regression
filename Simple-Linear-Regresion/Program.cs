1df78e51a4bb2281368c433fd303a9c34fa4bead
Rovehacks <driveror31@gmail.com>
2024-11-14 17:50:11 -0600

    Separate the program in classes

---
+++ Simple-Linear-Regresion/Program.cs
@@ -0,0 +1,104 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Dataset dataset = new Dataset(new (float, float )[]
+{
+  (1, 2),
+  (2, 4),
+  (3, 6),
+  (4, 8),
+  (5, 10),
+  (6, 12),
+  (7, 14),
+  (8, 16),
+  (9, 18)
+});
+
+SLR slr = new SLR(dataset);
+
+while(true)
+{
+  Console.WriteLine("Ingrese un valor para X (Sales)");
+  var x = Convert.ToDouble(Console.ReadLine());
+  Console.WriteLine($"El valor de 'y' (Advertising) es de {slr.GetFor(x)}");
+}
+
+
+
+public class SLR
+{
+  private Model model;
+  public SLR(Dataset dataset)
+  {
+    model = new Model(dataset);
+  }
+  public double GetFor(double x) => model.b0 + (model.b1 * x);
+}
+
+ static class DiscMaths
+  {
+    public static float getXSummation(Dataset dataset)
+    {
+      float result = 0;
+
+      foreach(var tuple in dataset.Data)
+        result += tuple.X;
+
+      return result;
+    }
+    public static float getYSummation(Dataset dataset)
+    {
+      float result = 0;
+
+      foreach(var tuple in dataset.Data)
+        result += tuple.Y;
+
+      return result;
+    }
+    public static float getSquaredXSummation(Dataset dataset)
+    {
+      float result = 0;
+
+      foreach(var tuple in dataset.Data)
+        result+= (tuple.X*tuple.X);
+  
+      return result;
+    }
+    public static float getXYSummation(Dataset dataset)
+    {
+      float result = 0;
+
+      foreach(var tuple in dataset.Data)
+        result+= (tuple.X*tuple.Y);
+  
+      return result;
+    }
+  }
+  class Model()
+  {
+    public readonly double b0;
+    public readonly double b1;
+
+    private double n;
+    private double xSummation;
+    private double ySummation;
+    private double x2Summation;
+    private double xySummation;
+
+    public Model(Dataset dataset) : this()
+    {
+      n = dataset.Data.Length;
+      xSummation = DiscMaths.getXSummation(dataset);
+      ySummation = DiscMaths.getYSummation(dataset);
+      x2Summation = DiscMaths.getSquaredXSummation(dataset);
+      xySummation = DiscMaths.getXYSummation(dataset);
+
+      b0 = ( (x2Summation * ySummation) - (xSummation * xySummation) ) / ( (n * x2Summation) - (xSummation * xSummation) );
+      b1 = ( (n * xySummation) - (xSummation * ySummation) ) / ( (n * x2Summation) - (xSummation * xSummation) );
+      Console.WriteLine($"y = {b0} + {b1}x");
+    }
+  }
+
+public class Dataset((float,float)[] data)
+{
+  public (float X, float Y)[] Data { get; } = data;
+}
---
+++ Simple-Linear-Regresion/README.md
@@ -0,0 +1,25 @@
+# Hands-on 2
+Topic: Curve Fitting with SLR
+
+---
+
+A partir del modelo matemático de análisis predictivo Simple Linear Regression(SLR):
+
+1. Implementar el modelo, orientado a objetos, a través del lenguaje de programación que les resulte más conveniente. Esto es, se deben implementar las dos fórmulas construidas en clase.
+
+2. Dado el dataset del caso Benetton, calcular los valores óptimos para los parámetros del modelo predictivo (Beta_0 y Beta_1).
+
+3. Sólo utilizarán las columnas Sales y Advertising.
+
+---
+
+## Hands-on 2: Specs
+
+### Entradas de la aplicación:
+- Un conjunto de observaciones o dataset: $(x_1,y_1),(x_2,y_2),...,(x_n,y_n)$
+- El dataset estará hardcoded (declarado a través de un constructor).
+- No se capturan valores por predecir; se inyectan a través de la terminal (Command Prompt) al ejecutar la clase main.
+
+### Salidas de la aplicación:
+- Imprimir la ecuación de regresión: $y = b_0 + b_1 x_1$ reemplazando los valores óptimos, calculados, para los parámetros Beta.
+- Inyectar valores, por predecir, a través del intérprete de comandos.
